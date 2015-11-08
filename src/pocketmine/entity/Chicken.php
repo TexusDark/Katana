380741f2c9a692649db4baa4bcd53885b906bcec
TexusDark <texusdark.offcial@gmail.com>
2015-11-08 14:48:31 +0100

    Added Chicken

--- src/pocketmine/entity/Chicken.php
+++ src/pocketmine/entity/Chicken.php
@@ -21,7 +21,46 @@
 
 namespace pocketmine\entity;
 
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\item\Item as ItemItem;
+use pocketmine\network\Network;
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\Player;
 
 class Chicken extends Animal{
+        const NETWORK_ID = 10;
+        
+	      public $width = 0.5;
+	      public $length = 0.8;
+	      public $height = 0.9;
+	      
+	      public function getName(){
+		            return "Chicken";
+	      }
+	      
+	      public function spawnTo(Player $player){
+		            $pk = new AddEntityPacket();
+		            $pk->eid = $this->getId();
+		            $pk->type = Chicken::NETWORK_ID;
+		            $pk->x = $this->x;
+		            $pk->y = $this->y;
+		            $pk->z = $this->z;
+		            $pk->speedX = $this->motionX;
+		            $pk->speedY = $this->motionY;
+		            $pk->speedZ = $this->motionZ;
+		            $pk->yaw = $this->yaw;
+		            $pk->pitch = $this->pitch;
+		            $pk->metadata = $this->dataProperties;
+		            $player->dataPacket($pk);
+		            
+		            parent::spawnTo($player);
+	      }
+	
+	      public function getDrops(){
+		            $drops = [
+			                  ItemItem::get(ItemItem::FEATHER, 0, 1);
+			                  ItemItem::get(ItemItem::RAW_CHICKEN, 0, mt_rand(1, 2));
+		            ];
+	      }
 
 }
